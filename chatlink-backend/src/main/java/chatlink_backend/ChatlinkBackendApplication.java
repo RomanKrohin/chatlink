ae7f7ffa9da3dbef69f9c9adc4fa2cfdfb31d4f4
Roman <romankrohin126@gmail.com>
2024-07-22 20:25:22 +0300

    chore: init docker conf, init gradle conf adn dep

---
+++ chatlink-backend/src/main/java/chatlink_backend/ChatlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package chatlink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatlinkBackendApplication.class, args);
+	}
+
+}
